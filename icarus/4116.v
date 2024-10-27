98c5d36ed0d13cc0ceb20384f3f1381ab7c7490c
org <ogamespec@gmail.com>
2024-10-27 17:40:17 +0300

    Update 4116.v

--- icarus/4116.v
+++ icarus/4116.v
@@ -1,5 +1,50 @@
 // Model for the classic MOSTEK 4116 16384 x 1 Bit Dynamic Ram
 
-module MK4116 ();
+module MK4116 (Din, Dout, nWRITE, nRAS, nCAS, A);
+
+	input wire Din;
+	output wire Dout;
+	input wire nWRITE;
+	input wire nRAS;
+	input wire nCAS;
+	input wire [6:0] A;
+
+	reg memory [16383:0];
+	reg [6:0] row_latch;
+	reg [6:0] col_latch;
+	wire mem_addr;
+
+	// Latch Row and Column
+
+	always @ (negedge nRAS) begin
+		row_latch <= A;
+	end
+
+	always @ (negedge nCAS) begin
+		col_latch <= A;
+	end
+
+	assign mem_addr = {row_latch, col_latch};
+
+	// READ CYCLE
+
+	always @ (posedge nCAS) begin
+		if (nWRITE) begin
+			Dout <= memory[mem_addr];
+		end
+	end
+
+	// WRITE CYCLE (EARLY WRITE)
+
+	always @ (negedge nCAS) begin
+		if (! nWRITE) begin
+			memory[mem_addr] <= Din;
+		end
+	end
+
+	// TBD: READ-WRITE/READ-MODIFY-WRITE CYCLE
+	// TBD: RAS-ONLY REFRESH CYCLE
+	// TBD: PAGE MODE READ CYCLE
+	// TBD: PAGE MODE WRITE CYCLE
 
 endmodule // MK4116
